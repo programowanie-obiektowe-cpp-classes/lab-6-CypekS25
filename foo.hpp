c252a2e3f350a9f46a3d86bfd6f2c1cefb7e06c2
CypekS25 <CypSto@gmail.com>
2025-11-30 19:14:04 +0100

    solved

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,12 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::size_t idx = retval.size(); 
+    std::for_each(people.begin(), people.end(), [&](Human& h) {
+        h.birthday();                              
+        char c = h.isMonster() ? 'n' : 'y'; 
+        retval[--idx] = c; // zapis od koñca wektora
+    });
 
     return retval;
 }
